afb723cf38ce6a6291128c9b3f0b439bc0063e26
gurish <gurish@umich.edu>
2023-02-22 04:06:57 +0100

    i think I can successfully extract all the characters

--- Pokemon_Duel_Scraper/main.py
+++ Pokemon_Duel_Scraper/main.py
@@ -8,6 +8,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
+def add_to_set_from_file(file_name, name_set=None):
+    if name_set is None:
+        name_set = set()
+    try:
+        with open(file_name, "r") as file:
+            for line in file:
+                name = line.strip()
+                name_set.add(name)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File {file_name} does not exist in current directory")
+    return name_set
+
 def setup_chromedriver():
     chrome_options = Options()
     chrome_options.add_argument("--headless") # Ensure GUI is off
@@ -77,55 +89,66 @@ def extract_header_info(driver):
 
 
 def extract_attack_info(driver):
-  # Wait for the table to load
-    table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'dextable')))
+    # Wait for the table to load
+    try:
+        table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'dextable')))
     
-    # Extract the relevant information from the HTML
-    info_list = []
-    for tr in table.find_all('tr')[1:]:
-        info = {}
-        tds = tr.find_all('td')
-        info['Wheel Size'] = tds[0].get_text().strip()
-        info['Name'] = tds[1].get_text().strip()
-        info['Attack Type'] = tds[2].get_text().strip()
-        info['Additional Notes'] = tds[3].get_text().strip()
-        info['Damage'] = tds[4].get_text().strip()
-        if info['Attack Type'] != "White Z-Move" or info['Move Type'] != "Purple Z-Move":
-            info_list.append(info)
+        # Extract the relevant information from the HTML
+        info_list = []
+        for tr in table.find_elements(By.TAG_NAME, 'tr')[1:]:
+            info = {}
+            tds = tr.find_elements(By.TAG_NAME, 'td')
+            info['Wheel Size'] = tds[0].text.strip()
+            info['Name'] = tds[1].text.strip()
+            info['Attack Type'] = tds[2].text.strip()
+            info['Additional Notes'] = tds[3].text.strip()
+            info['Damage'] = tds[4].text.strip()
+            if info['Attack Type'] != "White Z-Move" and info['Attack Type'] != "Purple Z-Move":
+                info_list.append(info)
 
-    # Return the list of dictionaries
-    return info_list
+        # Return the list of dictionaries
+        return info_list
+    except:
+        return []
 
 
 def process_pokemon_urls(url_list):
     # Set up the pandas DataFrame with the specified columns
-    columns = ['Name', 'Movement', 'Rarity', 'Type', 'Special Ability', 'Attack Wheel Size', 'Attack Name', 'Attack Type', 'Attack Value', 'Attack Ability']
+    columns = ['Name', 'Movement', 'Rarity', 'Type', 'Special Ability', 
+               'Attack Wheel Size', 'Attack Name', 'Attack Type', 'Attack Value', 
+               'Attack Ability', 'is Gen 1']
     result_df = pd.DataFrame(columns=columns)
 
     # Set up the web driver
-    driver = webdriver.Chrome()  # Change this to the path of your Chrome driver if needed
+    print("Setting up Chromedriver...")
+    driver = setup_chromedriver()  # Change this to the path of your Chrome driver if needed
+
+    # load up pokemon generation
+    gen_1_set = add_to_set_from_file("Gen_1_pokemon.txt")
 
     for href in url_list:
         # Open the webpage
+        print(f"Opening webpage {href}...")
         driver.get(href)
 
         # Extract the desired information from the webpage and add it to the pandas DataFrame
         name = extract_pokemon_name(driver)
         movement, rarity, type, special_ability = extract_header_info(driver)
+        is_gen_1 = True if name in gen_1_set else False
         attack_info_list = extract_attack_info(driver) # attack_info_list is a list of dictionaries
         # append rows to result_df
         for attack in attack_info_list:
-          attack_wheel_size = attack['Wheel Size']
-          attack_name = attack['Name']
-          attack_move_type = attack['Attack Type']
-          attack_add_notes = ['Additional Notes']
-          attack_damage = ['Damage']
-          row = [name, movement, rarity, type, special_ability, attack_wheel_size, attack_name, attack_move_type, attack_add_notes, attack_damage]
-          result_df.loc[len(result_df)] = row
+            attack_wheel_size = attack['Wheel Size']
+            attack_name = attack['Name']
+            attack_move_type = attack['Attack Type']
+            attack_add_notes = attack['Additional Notes']
+            attack_damage = attack['Damage']
+            row = [name, movement, rarity, type, special_ability, attack_wheel_size, attack_name, attack_move_type, attack_add_notes, attack_damage, is_gen_1]
+            result_df.loc[len(result_df)] = row
 
     # Clean up
     driver.quit()
-
+    
     return result_df
 
 def save_df_as_csv(df):
@@ -133,9 +156,13 @@ def save_df_as_csv(df):
     folder_name = "Pokemon_Duel_Characters"
     file_name_prefix = "pokemon_duel_characters_v"
 
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
     # Get the list of existing file names and determine the latest version number
     file_names = os.listdir(folder_name)
-    version_numbers = [int(file_name[len(file_name_prefix):]) for file_name in file_names if file_name.startswith(file_name_prefix)]
+    version_numbers = [int(file_name[len(file_name_prefix):-4]) for file_name in file_names if file_name.startswith(file_name_prefix)]
     if len(version_numbers) > 0:
         latest_version_number = max(version_numbers)
     else:
@@ -145,10 +172,6 @@ def save_df_as_csv(df):
     new_version_number = latest_version_number + 1
     new_file_name = f"{file_name_prefix}{new_version_number}.csv"
 
-    # Create the folder if it doesn't exist
-    if not os.path.exists(folder_name):
-        os.mkdir(folder_name)
-
     # Export the pandas dataframe as a CSV file with the new file name
     df.to_csv(os.path.join(folder_name, new_file_name), index=False)
 
@@ -156,10 +179,11 @@ def save_df_as_csv(df):
 def main():
   # Get all links to every Pokemon
   pokemon_duel_listing_url = "https://www.serebii.net/duel/figures.shtml"
-  href_list = get_hrefs_from_table(pokemon_duel_listing_url)
-  print(href_list[:10])
-#   df = process_pokemon_urls(href_list)
-#   save_df_as_csv(df)
+#   href_list = get_hrefs_from_table(pokemon_duel_listing_url)
+#   print(href_list[:10])
+  df = process_pokemon_urls(["https://www.serebii.net/duel/figures/614-trubbish.shtml"])
+#   print(df)
+  save_df_as_csv(df)
   
 
 if __name__ == "__main__":
